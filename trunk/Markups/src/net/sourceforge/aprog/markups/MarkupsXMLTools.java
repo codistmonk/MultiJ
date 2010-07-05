7ce4a7da03f1572d3739b99298ec49262caa6b10
codistmonk <codistmonk@ee575a8f-e4d0-496e-93aa-ac9a83b99b23>
2010-07-05 14:06:31 +0000

    [Markups] Removed contents from MarkupsXMLTools (moved into Aprog's XMLTools).

--- trunk/Markups/src/net/sourceforge/aprog/markups/MarkupsXMLTools.java
+++ trunk/Markups/src/net/sourceforge/aprog/markups/MarkupsXMLTools.java
@@ -24,17 +24,8 @@
 
 package net.sourceforge.aprog.markups;
 
-import static java.util.Arrays.asList;
-import static java.util.Collections.unmodifiableList;
-
-import java.util.List;
 
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
-import net.sourceforge.aprog.xml.XMLTools;
-
-import org.w3c.dom.Node;
-import org.w3c.dom.events.EventListener;
-import org.w3c.dom.events.EventTarget;
 
 /**
  *
@@ -48,98 +39,5 @@ public final class MarkupsXMLTools {
     private MarkupsXMLTools() {
         throw new IllegalInstantiationException();
     }
-
-    /**
-     *
-     * @param node
-     * <br>Not null
-     * <br>Input-output
-     * @param listener
-     * <br>Not null
-     * <br>Shared
-     * @throws IllegalArgumentException If {@code node} doesn't have the "events 2.0" feature
-     */
-    public static final void addDOMEventListener(final Node node, final EventListener listener) {
-        checkHasEventsFeature(node);
-
-        for (final String eventType : DOM_EVENT_TYPES) {
-            ((EventTarget) node).addEventListener(eventType, listener, false);
-        }
-    }
-
-    /**
-     *
-     * @param node
-     * <br>Not null
-     * <br>Input-output
-     * @param listener
-     * <br>Not null
-     * <br>Shared
-     * @throws IllegalArgumentException If {@code node} doesn't have the "events 2.0" feature
-     */
-    public static final void removeDOMEventListener(final Node node, final EventListener listener) {
-        checkHasEventsFeature(node);
-
-        for (final String eventType : DOM_EVENT_TYPES) {
-            ((EventTarget) node).removeEventListener(eventType, listener, false);
-        }
-    }
-
-    /**
-     *
-     * @param node
-     * <br>Not null
-     * @throws IllegalArgumentException If {@code node} doesn't have the "events 2.0" feature
-     */
-    public static final void checkHasEventsFeature(final Node node) {
-        if (!XMLTools.getOwnerDocument(node).getImplementation().hasFeature("events", "2.0")) {
-            throw new IllegalArgumentException("Events 2.0 feature unavailable for node " + node);
-        }
-    }
-
-    /**
-     * {@value}.
-     */
-    public static final String DOM_EVENT_SUBTREE_MODIFIED = "DOMSubtreeModified";
-
-    /**
-     * {@value}.
-     */
-    public static final String DOM_EVENT_NODE_INSERTED = "DOMNodeInserted";
-
-    /**
-     * {@value}.
-     */
-    public static final String DOM_EVENT_NODE_REMOVED = "DOMNodeRemoved";
-
-    /**
-     * {@value}.
-     */
-    public static final String DOM_EVENT_NODE_REMOVED_FROM_DOCUMENT = "DOMNodeRemovedFromDocument";
-
-    /**
-     * {@value}.
-     */
-    public static final String DOM_EVENT_NODE_INSERTED_INTO_DOCUMENT = "DOMNodeInsertedIntoDocument";
-
-    /**
-     * {@value}.
-     */
-    public static final String DOM_EVENT_ATTRIBUTE_MODIFIED = "DOMAttrModified";
-
-    /**
-     * {@value}.
-     */
-    public static final String DOM_EVENT_CHARACTER_DATA_MODIFIED = "DOMCharacterDataModified";
-
-    public static final List<String> DOM_EVENT_TYPES = unmodifiableList(asList(
-            DOM_EVENT_ATTRIBUTE_MODIFIED,
-            DOM_EVENT_CHARACTER_DATA_MODIFIED,
-            DOM_EVENT_NODE_INSERTED,
-            DOM_EVENT_NODE_INSERTED_INTO_DOCUMENT,
-            DOM_EVENT_NODE_REMOVED,
-            DOM_EVENT_NODE_REMOVED_FROM_DOCUMENT,
-            DOM_EVENT_SUBTREE_MODIFIED
-    ));
 
 }
