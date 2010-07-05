6059412ca1fb14930b5d200033d2fd4a0e66a292
codistmonk <codistmonk@ee575a8f-e4d0-496e-93aa-ac9a83b99b23>
2010-07-05 14:06:31 +0000

    [Markups] Removed contents from MarkupsXMLTools (moved into Aprog's XMLTools).

--- Markups/test/net/sourceforge/aprog/markups/MarkupsXMLToolsTest.java
+++ Markups/test/net/sourceforge/aprog/markups/MarkupsXMLToolsTest.java
@@ -24,56 +24,13 @@
 
 package net.sourceforge.aprog.markups;
 
-import static net.sourceforge.aprog.events.EventsTestingTools.*;
-import static net.sourceforge.aprog.xml.XMLTools.*;
-
 import static org.junit.Assert.*;
-
-import org.junit.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.events.Event;
-import org.w3c.dom.events.EventListener;
-import org.w3c.dom.events.MutationEvent;
 
 /**
  * Automated tests using JUnit 4 for {@link MarkupsXMLTools}.
- * <br>WARNING: This test reuses part of Aprog's tests, so Aprog's tests classes must be compiled before
- * trying to compile or run this test.
  *
  * @author codistmonk (creation 2010-07-04)
  */
 public final class MarkupsXMLToolsTest {
-
-    @Test
-    public final <R extends EventRecorder<Event> & EventListener> void testEvents() {
-        final Document document = parse("<a><b c='d'/></a>");
-        @SuppressWarnings("unchecked")
-        final R recorder = (R) newEventRecorder(EventListener.class);
-
-        MarkupsXMLTools.addDOMEventListener(document, recorder);
-        getNode(document, "a/b/@c").setNodeValue("e");
-
-        {
-            final MutationEvent event = recorder.getEvent(0);
-
-            assertEquals(MarkupsXMLTools.DOM_EVENT_ATTRIBUTE_MODIFIED, event.getType());
-            assertSame(getNode(document, "a/b"), event.getTarget());
-            assertEquals(MutationEvent.MODIFICATION, event.getAttrChange());
-            assertEquals("d", event.getPrevValue());
-            assertEquals("e", event.getNewValue());
-        }
-
-        {
-            final MutationEvent event = recorder.getEvent(1);
-
-            assertEquals(MarkupsXMLTools.DOM_EVENT_SUBTREE_MODIFIED, event.getType());
-            assertSame(getNode(document, "a/b"), event.getTarget());
-            assertEquals(0, event.getAttrChange());
-            assertNull(event.getPrevValue());
-            assertNull(event.getNewValue());
-        }
-
-        assertEquals(2, recorder.getEvents().size());
-    }
 
 }
