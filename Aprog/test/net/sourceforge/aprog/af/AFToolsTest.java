0994fe3ecaa0ac0184662342188b916c8ef4c7b1
codistmonk <codistmonk@ee575a8f-e4d0-496e-93aa-ac9a83b99b23>
2010-10-16 09:46:34 +0000

    [Aprog] Added package net.sourceforge.aprog.af + tests.

---
+++ Aprog/test/net/sourceforge/aprog/af/AFToolsTest.java
@@ -0,0 +1,210 @@
+/*
+ *  The MIT License
+ * 
+ *  Copyright 2010 Codist Monk.
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+package net.sourceforge.aprog.af;
+
+import static org.junit.Assert.*;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JTabbedPane;
+import javax.swing.KeyStroke;
+
+import net.sourceforge.aprog.context.Context;
+import net.sourceforge.aprog.swing.SwingTools;
+import net.sourceforge.aprog.tools.IllegalInstantiationException;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author codistmonk (creation 2010-10-16)
+ */
+public final class AFToolsTest {
+
+    @Test
+    public final void testSetupSystemLookAndFeel() {
+        AFTools.setupSystemLookAndFeel(this.getClass().getSimpleName());
+
+        if (MacOSXTools.MAC_OS_X) {
+            assertTrue(MacOSXTools.getUseScreenMenuBar());
+            assertEquals(this.getClass().getSimpleName(), MacOSXTools.getApplicationName());
+        }
+    }
+
+    @Test
+    public final void testNewContext() {
+        final Context context = AFTools.newContext();
+
+        assertNotNull(context);
+        assertEquals(AFConstants.APPLICATION_NAME, context.get(AFConstants.Variables.APPLICATION_NAME));
+        assertEquals(AFConstants.APPLICATION_VERSION, context.get(AFConstants.Variables.APPLICATION_VERSION));
+        assertEquals(AFConstants.APPLICATION_COPYRIGHT, context.get(AFConstants.Variables.APPLICATION_COPYRIGHT));
+        assertEquals(AFConstants.APPLICATION_ICON_PATH, context.get(AFConstants.Variables.APPLICATION_ICON_PATH));
+    }
+
+    @Test
+    public final void testNewAboutItem() {
+        if (SwingTools.canInvokeThisMethodInAWT(this)) {
+            final Context context = AFTools.newContext();
+
+            if (MacOSXTools.MAC_OS_X && MacOSXTools.getUseScreenMenuBar()) {
+                assertEquals(null, AFTools.newAboutItem(context));
+            } else {
+                assertNotNull(AFTools.newAboutItem(context));
+            }
+
+            final ShowAboutDialogAction action = context.get(AFConstants.Variables.ACTIONS_SHOW_ABOUT_DIALOG);
+
+            assertNotNull(action);
+        }
+    }
+
+    @Test
+    public final void testNewPreferencesItem() {
+        if (SwingTools.canInvokeThisMethodInAWT(this)) {
+            final Context context = AFTools.newContext();
+
+            if (MacOSXTools.MAC_OS_X && MacOSXTools.getUseScreenMenuBar()) {
+                assertEquals(null, AFTools.newPreferencesItem(context));
+            } else {
+                assertNotNull(AFTools.newPreferencesItem(context));
+            }
+
+            final ShowPreferencesDialogAction action = context.get(AFConstants.Variables.ACTIONS_SHOW_PREFERENCES_DIALOG);
+
+            assertNotNull(action);
+        }
+    }
+
+    @Test
+    public final void testNewQuitItem() {
+        if (SwingTools.canInvokeThisMethodInAWT(this)) {
+            final Context context = AFTools.newContext();
+
+            if (MacOSXTools.MAC_OS_X && MacOSXTools.getUseScreenMenuBar()) {
+                assertEquals(null, AFTools.newQuitItem(context));
+            } else {
+                assertNotNull(AFTools.newQuitItem(context));
+            }
+
+            final QuitAction action = context.get(AFConstants.Variables.ACTIONS_QUIT);
+
+            assertNotNull(action);
+        }
+    }
+
+    @Test
+    public final void testPerform() {
+        final Context context = new Context();
+
+        new AbstractAFAction(context, Variables.TEST) {
+
+            @Override
+            public final void perform() {
+                context.set(Variables.SUCCESS, true);
+            }
+
+        };
+
+        AFTools.perform(context, Variables.TEST);
+
+        assertEquals(true, context.get(Variables.SUCCESS));
+    }
+
+    @Test
+    public final void testIndexOf() {
+        if (SwingTools.canInvokeThisMethodInAWT(this)) {
+            final JTabbedPane tabbedPane = new JTabbedPane();
+            final JComponent component = new JLabel();
+
+            tabbedPane.addTab("test", component);
+
+            assertEquals(0, AFTools.indexOf(tabbedPane, component));
+            assertEquals(-1, AFTools.indexOf(tabbedPane, new JLabel()));
+        }
+    }
+
+    @Test
+    public final void testMenu() {
+        if (SwingTools.canInvokeThisMethodInAWT(this)) {
+            assertNotNull(AFTools.menu("test"));
+        }
+    }
+
+    @Test
+    public final void testItem() {
+        if (SwingTools.canInvokeThisMethodInAWT(this)) {
+            assertNotNull(AFTools.item("test", new Context(), Variables.TEST));
+            assertNotNull(AFTools.item("test", KeyStroke.getKeyStroke(AFTools.META + " + A"), new Context(), Variables.TEST));
+        }
+    }
+
+    @Test
+    public final void testNewListener() {
+        final Context context = new Context();
+        final Context.Listener listener = AFTools.newListener(Context.Listener.class, "variableAdded",
+                this.getClass(), "updateSuccess", context);
+
+        context.addListener(listener);
+        context.set(Variables.SUCCESS, false);
+
+        assertEquals(true, context.get(Variables.SUCCESS));
+    }
+
+    /**
+     *
+     * @param context
+     * <br>Not null
+     * <br>Input-output
+     */
+    public static final void updateSuccess(final Context context) {
+        context.set(Variables.SUCCESS, true);
+    }
+
+    /**
+     * @author codistmonk (creation 2010-10-16)
+     */
+    public static final class Variables {
+
+        /**
+         * @throws IllegalInstantiationException To prevent instantiation
+         */
+        private Variables() {
+            throw new IllegalInstantiationException();
+        }
+
+        /**
+         * {@value}.
+         */
+        public static final String SUCCESS = "success";
+
+        /**
+         * {@value}.
+         */
+        public static final String TEST = "test";
+
+    }
+
+}
