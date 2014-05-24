cf1aa9268be91e45e51bde2bef0021781a2804f2
codistmonk <codistmonk@ee575a8f-e4d0-496e-93aa-ac9a83b99b23>
2014-05-24 19:40:21 +0000

    [Aprog] Added ConsoleMonitor in package tools.

--- trunk/Aprog/src/net/sourceforge/aprog/tools/TicToc.java
+++ trunk/Aprog/src/net/sourceforge/aprog/tools/TicToc.java
@@ -1,3 +1,27 @@
+/*
+ *  The MIT License
+ * 
+ *  Copyright 2013 Codist Monk.
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
 package net.sourceforge.aprog.tools;
 
 import static java.lang.System.currentTimeMillis;
@@ -25,6 +49,7 @@ public final class TicToc {
 	public final long tic() {
 		this.totalTime += this.ticTocTime;
 		this.ticTocTime = 0L;
+		
 		return this.t0 = currentTimeMillis();
 	}
 	
@@ -32,9 +57,20 @@ public final class TicToc {
 	 * @return The time in milliseconds since the last call to {@link #tic()}
 	 */
 	public final long toc() {
-		this.ticTocTime = currentTimeMillis() - this.t0;
+		return this.ticTocTime = currentTimeMillis() - this.t0;
+	}
+	
+	/**
+	 * Calls {@link #toc()} and then {@link #tic()}.
+	 * 
+	 * @return The result of the call to {@link #toc()}
+	 */
+	public final long toctic() {
+		final long result = this.toc();
 		
-		return this.ticTocTime;
+		this.tic();
+		
+		return result;
 	}
 	
 	/**
---
+++ trunk/Aprog/test/net/sourceforge/aprog/tools/ConsoleMonitorTest.java
@@ -0,0 +1,72 @@
+/*
+ *  The MIT License
+ * 
+ *  Copyright 2014 Codist Monk.
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+package net.sourceforge.aprog.tools;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import net.sourceforge.aprog.tools.ConsoleMonitor.MessageBuilder;
+
+import org.junit.Test;
+
+/**
+ * @author codistmonk (creation 2014-05-24)
+ */
+public class ConsoleMonitorTest {
+	
+	@Test
+	public final void test() {
+		final AtomicInteger count = new AtomicInteger();
+		final ConsoleMonitor monitor = new ConsoleMonitor(100L).setMessageBuilder(new MessageBuilder() {
+			
+			@Override
+			public final String getMessage() {
+				count.incrementAndGet();
+				return MessageBuilder.Predefined.DOT.getMessage();
+			}
+			
+			/**
+			 * {@value}.
+			 */
+			private static final long serialVersionUID = 4881389430607213459L;
+			
+		});
+		
+		monitor.ping();
+		monitor.ping();
+		
+		assertEquals(0L, count.get());
+		
+		Tools.gc(100);
+		
+		monitor.ping();
+		
+		assertEquals(1L, count.get());
+		
+		monitor.pause();
+	}
+	
+}
