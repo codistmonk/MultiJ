9ed937885f3611bfe04a6a64d27319f0ffab9832
codistmonk <codistmonk@ee575a8f-e4d0-496e-93aa-ac9a83b99b23>
2015-03-04 06:48:19 +0000

    [Aprog] Updated IntComparator.

--- Aprog/src/net/sourceforge/aprog/tools/IntComparator.java
+++ Aprog/src/net/sourceforge/aprog/tools/IntComparator.java
@@ -1,3 +1,27 @@
+/*
+ *  The MIT License
+ *
+ *  Copyright 2015 Codist Monk.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
 package net.sourceforge.aprog.tools;
 
 import java.io.Serializable;
@@ -16,7 +40,7 @@ public abstract interface IntComparator extends Serializable {
 		
 		@Override
 		public final int compare(final int value1, final int value2) {
-			return value1 < value2 ? -1 : value1 == value2 ? 0 : 1;
+			return Integer.compare(value1, value2);
 		}
 		
 		/**
