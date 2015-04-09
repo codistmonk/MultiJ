f8850c04b3224e0c2d77e14ebb8bbfd4bc410062
codistmonk <codistmonk@ee575a8f-e4d0-496e-93aa-ac9a83b99b23>
2015-04-09 09:26:00 +0000

    [Aprog] Added Scripting tool.

---
+++ trunk/Aprog/src/net/sourceforge/aprog/tools/Scripting.java
@@ -0,0 +1,371 @@
+/*
+ *  The MIT License
+ * 
+ *  Copyright 2015 Codist Monk.
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+package net.sourceforge.aprog.tools;
+
+import static net.sourceforge.aprog.tools.Tools.ignore;
+import static net.sourceforge.aprog.tools.Tools.unchecked;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ * @author codistmonk (creation 2015-04-09)
+ */
+public final class Scripting implements Serializable {
+	
+	private final ScriptEngine scriptEngine;
+	
+	private final Scripting.Importer importer;
+	
+	public Scripting() {
+		this("JavaScript");
+	}
+	
+	/**
+	 * @param engineName
+	 * <br>Must not be null
+	 */
+	public Scripting(final String engineName) {
+		this.scriptEngine = getEngine(engineName);
+		this.importer = new Importer(this.scriptEngine, ScriptContext.ENGINE_SCOPE);
+	}
+	
+	/**
+	 * @param command
+	 * <br>Must not be null
+	 * @return
+	 * <br>Maybe null
+	 * <br>Maybe new
+	 */
+	public final Object eval(final String command) {
+		try {
+			return this.getScriptEngine().eval(command);
+		} catch (final ScriptException exception) {
+			throw unchecked(exception);
+		}
+	}
+	
+	/**
+	 * @return
+	 * <br>Not null
+	 * <br>Strong reference in <code>this</code>
+	 */
+	public final ScriptEngine getScriptEngine() {
+		return this.scriptEngine;
+	}
+	
+	/**
+	 * @param keys
+	 * <br>Must not be null
+	 */
+	public final void importAll(final Object... keys) {
+		this.importer.importAll(keys);
+	}
+	
+	/**
+	 * @param key
+	 * <br>Must not be null
+	 */
+	public final void importPackage(final Object key) {
+		this.importer.importPackage(key);
+	}
+	
+	/**
+	 * @param key
+	 * <br>Must not be null
+	 */
+	public final void importClass(final Object key) {
+		this.importer.importClass(key);
+	}
+	
+	private static final long serialVersionUID = -3104607045331464557L;
+	
+	/**
+	 * @param preferredEngineName
+	 * <br>Must not be null
+	 * @return
+	 * <br>Maybe null
+	 */
+	public static final ScriptEngine getEngine(final String preferredEngineName) {
+		final ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+		ScriptEngine result = scriptEngineManager.getEngineByName(preferredEngineName);
+		
+		if (result == null) {
+			final List<ScriptEngineFactory> engineFactories = scriptEngineManager.getEngineFactories();
+			
+			if (!engineFactories.isEmpty()) {
+				result = engineFactories.get(0).getScriptEngine();
+			}
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * @author codistmonk (creation 2015-04-09)
+	 */
+	public static final class Importer implements Bindings, Serializable {
+		
+		private final ScriptEngine scriptEngine;
+		
+		private final Map<String, Object> map;
+		
+		private final Collection<String> imports;
+		
+		private final Collection<Thread> recursionFlags;
+		
+		/**
+		 * @param scriptEngine
+		 * <br>Must not be null
+		 * <br>Stored as strong reference in <code>this</code>
+		 * @param scope
+		 * <br>Range: any int
+		 */
+		public Importer(final ScriptEngine scriptEngine, final int scope) {
+			this.scriptEngine = scriptEngine;
+			this.map = scriptEngine.getBindings(scope);
+			this.imports = new HashSet<>();
+			this.recursionFlags = new HashSet<>();
+			
+			if (!scriptEngine.getBindings(scope).containsKey("importPackage")) {
+				this.put("importPackage", new Consumer<Object>() {
+					
+					@Override
+					public final void accept(final Object object) {
+						Importer.this.importPackage(object);
+					}
+					
+				});
+			}
+			
+			if (!scriptEngine.getBindings(scope).containsKey("importClass")) {
+				this.put("importClass", new Consumer<Object>() {
+					
+					@Override
+					public final void accept(final Object object) {
+						Importer.this.importClass(object);
+					}
+					
+				});
+			}
+			
+			scriptEngine.setBindings(this, scope);
+		}
+		
+		/**
+		 * @return
+		 * <br>Not null
+		 * <br>Strong reference in <code>this</code>
+		 */
+		public final ScriptEngine getScriptEngine() {
+			return this.scriptEngine;
+		}
+		
+		/**
+		 * @return
+		 * <br>Not null
+		 * <br>Strong reference in <code>this</code>
+		 */
+		public final Collection<String> getImports() {
+			return this.imports;
+		}
+		
+		/**
+		 * @param keys
+		 * <br>Must not be null
+		 */
+		public final void importAll(final Object... keys) {
+			for (final Object key : keys) {
+				final String string = key.toString();
+				
+				if (string.startsWith(PACKAGE_KEY)) {
+					this.importPackage(key);
+				} else {
+					this.importClass(key);
+				}
+			}
+		}
+		
+		/**
+		 * @param keys
+		 * <br>Must not be null
+		 */
+		public final void importPackage(final Object key) {
+			this.importWithoutPrefix(key, PACKAGE_KEY);
+		}
+		
+		/**
+		 * @param keys
+		 * <br>Must not be null
+		 */
+		public final void importClass(final Object key) {
+			this.importWithoutPrefix(key, CLASS_KEY);
+		}
+		
+		@Override
+		public final boolean containsKey(final Object key) {
+			if (!this.map.containsKey(key)) {
+				final Thread flag = Thread.currentThread();
+				final boolean flagSetInCurrentFrame = this.recursionFlags.add(flag);
+				
+				try {
+					if (flagSetInCurrentFrame) {
+						for (final String prefix : getImports()) {
+							if (Package.getPackage(prefix) != null && this.bind(prefix + "." + key, prefix, key)
+									|| this.bind(prefix, prefix, key)) {
+								return true;
+							}
+						}
+					}
+					
+					return false;
+				} finally {
+					if (flagSetInCurrentFrame) {
+						this.recursionFlags.remove(flag);
+					}
+				}
+			}
+			
+			return true;
+		}
+		
+		@Override
+		public final Set<String> keySet() {
+			return this.map.keySet();
+		}
+		
+		@Override
+		public final Object put(final String key, final Object value) {
+			return this.map.put(key, value);
+		}
+		
+		@Override
+		public final Object get(final Object key) {
+			return this.map.get(key);
+		}
+		
+		@Override
+		public final Set<Map.Entry<String, Object>> entrySet() {
+			return this.map.entrySet();
+		}
+		
+		@Override
+		public final int size() {
+			return this.map.size();
+		}
+		
+		@Override
+		public final boolean isEmpty() {
+			return this.map.isEmpty();
+		}
+		
+		@Override
+		public final boolean containsValue(final Object value) {
+			return this.map.containsValue(value);
+		}
+		
+		@Override
+		public final Object remove(final Object key) {
+			return this.map.remove(key);
+		}
+		
+		@Override
+		public final void putAll(final Map<? extends String, ? extends Object> m) {
+			this.map.putAll(m);
+		}
+		
+		@Override
+		public final void clear() {
+			this.map.clear();
+		}
+		
+		@Override
+		public final Collection<Object> values() {
+			return this.map.values();
+		}
+		
+		/**
+		 * @param key
+		 * <br>Must not be null
+		 * @param prefix
+		 * <br>Must not be null
+		 */
+		private final void importWithoutPrefix(final Object key, final String prefix) {
+			String name = key.toString();
+			final boolean prefixed = name.startsWith(prefix);
+			name = name.substring(prefixed ? prefix.length() : 0, name.length() - (prefixed ? 1 : 0));
+			
+			this.getImports().add(name);
+		}
+		
+		/**
+		 * @param className
+		 * <br>Must not be null
+		 * @param prefix
+		 * <br>Must not be null
+		 * @param key
+		 * <br>Must not be null
+		 * @return
+		 * <br>Range: any boolean
+		 */
+		private final boolean bind(final String className, final String prefix, final Object key) {
+			try {
+				Class.forName(className);
+				
+				final Object candidate = this.getScriptEngine().eval("Packages." + prefix + "." + key);
+				
+				if (candidate != null) {
+					this.map.put(key.toString(), candidate);
+					
+					return true;
+				}
+			} catch (final ClassNotFoundException | ScriptException exception) {
+				ignore(exception);
+			}
+			
+			return false;
+		}
+		
+		private static final long serialVersionUID = -4733030105076545625L;
+		
+		private static final String PACKAGE_KEY = "[JavaPackage ";
+		
+		private static final String CLASS_KEY = "JavaClassStatics[";
+		
+	}
+	
+}
---
+++ trunk/Aprog/test/net/sourceforge/aprog/tools/ScriptingTest.java
@@ -0,0 +1,86 @@
+/*
+ *  The MIT License
+ * 
+ *  Copyright 2015 Codist Monk.
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+package net.sourceforge.aprog.tools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Automated tests using JUnit 4 for {@link Scripting}.
+ *
+ * @author codistmonk (creation 2015-04-09)
+ */
+public final class ScriptingTest {
+	
+	@Test
+	public final void testEval1() {
+		final Scripting scripting = new Scripting();
+		
+		assertEquals(new Integer(2), scripting.eval("1+1"));
+	}
+	
+	@Test
+	public final void testEval2() {
+		final Scripting scripting = new Scripting();
+		
+		scripting.eval("importClass('" + this.getClass().getName() + "');");
+		
+		assertEquals(new Integer(9), scripting.eval("f(3)"));
+	}
+	
+	@Test
+	public final void testImportAll() {
+		final Scripting scripting = new Scripting();
+		
+		scripting.importAll(Tools.class.getName(), Tools.class.getPackage().getName());
+		
+		assertNotNull(scripting.eval("Tools"));
+		assertNotNull(scripting.eval("debugPrint"));
+	}
+	
+	@Test
+	public final void testImportPackage() {
+		final Scripting scripting = new Scripting();
+		
+		scripting.importAll(Tools.class.getPackage().getName());
+		
+		assertNotNull(scripting.eval("Tools"));
+	}
+	
+	@Test
+	public final void testImportClass() {
+		final Scripting scripting = new Scripting();
+		
+		scripting.importAll(Tools.class.getName());
+		
+		assertNotNull(scripting.eval("debugPrint"));
+	}
+	
+	public static final int f(final int x) {
+		return x * x;
+	}
+	
+}
