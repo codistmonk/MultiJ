569d326b495e4470cc08fe1fbda71a0a529a5ee8
codistmonk <codistmonk@users.noreply.github.com>
2016-06-08 10:22:18 +0200

    [tools][CommandLineArgumentsParser] Add more methods.

--- src/multij/tools/CommandLineArgumentsParser.java
+++ src/multij/tools/CommandLineArgumentsParser.java
@@ -79,23 +79,125 @@ public final class CommandLineArgumentsParser implements Serializable {
 		return toIntArray(values);
 	}
 	
-//	public final long get(final String key, final long defaultValue) {
-//		final String valueRepresentation = this.map.get(key);
-//		
-//		return valueRepresentation != null ? Long.decode(valueRepresentation) : defaultValue;
-//	}
-//	
-//	public final float get(final String key, final float defaultValue) {
-//		final String valueRepresentation = this.map.get(key);
-//		
-//		return valueRepresentation != null ? Float.parseFloat(valueRepresentation) : defaultValue;
-//	}
-//	
-//	public final double get(final String key, final double defaultValue) {
-//		final String valueRepresentation = this.map.get(key);
-//		
-//		return valueRepresentation != null ? Double.parseDouble(valueRepresentation) : defaultValue;
-//	}
+	public final long[] get(final String key, final long... defaultValue) {
+		final String valueRepresentation = this.map.get(key);
+		
+		if (valueRepresentation == null) {
+			return defaultValue;
+		}
+		
+		final List<Long> values = new ArrayList<>();
+		
+		for (final String rangeRepresentation : valueRepresentation.split(",")) {
+			final String[] rangeParameters = rangeRepresentation.split(":");
+			final long first = Long.decode(rangeParameters[0]);
+			final long step;
+			final long last;
+			
+			if (rangeParameters.length == 2) {
+				step = 1L;
+				last = Long.decode(rangeParameters[1]);
+			} else if (rangeParameters.length == 3) {
+				step = Long.decode(rangeParameters[1]);
+				last = Long.decode(rangeParameters[2]);
+			} else {
+				step = 1L;
+				last = first;
+			}
+			
+			values.addAll(newRange(first, step, last));
+		}
+		
+		return toLongArray(values);
+	}
+	
+	public final float[] get(final String key, final float... defaultValue) {
+		final String valueRepresentation = this.map.get(key);
+		
+		if (valueRepresentation == null) {
+			return defaultValue;
+		}
+		
+		final List<Float> values = new ArrayList<>();
+		
+		for (final String rangeRepresentation : valueRepresentation.split(",")) {
+			final String[] rangeParameters = rangeRepresentation.split(":");
+			final float first = decodeInt(rangeParameters[0]);
+			final float step;
+			final float last;
+			
+			if (rangeParameters.length == 2) {
+				step = 1;
+				last = Float.parseFloat(rangeParameters[1]);
+			} else if (rangeParameters.length == 3) {
+				step = Float.parseFloat(rangeParameters[1]);
+				last = Float.parseFloat(rangeParameters[2]);
+			} else {
+				step = 1;
+				last = first;
+			}
+			
+			values.addAll(newRange(first, step, last));
+		}
+		
+		return toFloatArray(values);
+	}
+	
+	public final double[] get(final String key, final double... defaultValue) {
+		final String valueRepresentation = this.map.get(key);
+		
+		if (valueRepresentation == null) {
+			return defaultValue;
+		}
+		
+		final List<Double> values = new ArrayList<>();
+		
+		for (final String rangeRepresentation : valueRepresentation.split(",")) {
+			final String[] rangeParameters = rangeRepresentation.split(":");
+			final double first = decodeInt(rangeParameters[0]);
+			final double step;
+			final double last;
+			
+			if (rangeParameters.length == 2) {
+				step = 1;
+				last = Double.parseDouble(rangeParameters[1]);
+			} else if (rangeParameters.length == 3) {
+				step = Double.parseDouble(rangeParameters[1]);
+				last = Double.parseDouble(rangeParameters[2]);
+			} else {
+				step = 1;
+				last = first;
+			}
+			
+			values.addAll(newRange(first, step, last));
+		}
+		
+		return toDoubleArray(values);
+	}
+	
+	public final int get1(final String key, final int defaultValue) {
+		final String valueRepresentation = this.map.get(key);
+		
+		return valueRepresentation == null ? defaultValue : decodeInt(valueRepresentation);
+	}
+	
+	public final long get1(final String key, final long defaultValue) {
+		final String valueRepresentation = this.map.get(key);
+		
+		return valueRepresentation == null ? defaultValue : Long.decode(valueRepresentation);
+	}
+	
+	public final float get1(final String key, final float defaultValue) {
+		final String valueRepresentation = this.map.get(key);
+		
+		return valueRepresentation == null ? defaultValue : Float.parseFloat(valueRepresentation);
+	}
+	
+	public final double get1(final String key, final double defaultValue) {
+		final String valueRepresentation = this.map.get(key);
+		
+		return valueRepresentation == null ? defaultValue : Double.parseDouble(valueRepresentation);
+	}
 	
 	public static final Map<String, String> toMap(final String[] commandLineArguments) {
 		final Map<String, String> result = new LinkedHashMap<>();
@@ -108,20 +210,80 @@ public final class CommandLineArgumentsParser implements Serializable {
 	}
 	
 	public static final List<Integer> newRange(final int first, final int step, final int last) {
-		final List<Integer> resultBuilder = new ArrayList<>();
+		final List<Integer> result = new ArrayList<>();
 		
 		for (int i = first; first <= last ? i <= last : last <= i; i += step) {
-			resultBuilder.add(i);
+			result.add(i);
 		}
 		
-		return resultBuilder;
+		return result;
 	}
 	
-	public static final int[] toIntArray(final List<Integer> resultBuilder) {
-		final int[] result = new int[resultBuilder.size()];
+	public static final List<Long> newRange(final long first, final long step, final long last) {
+		final List<Long> result = new ArrayList<>();
+		
+		for (long i = first; first <= last ? i <= last : last <= i; i += step) {
+			result.add(i);
+		}
+		
+		return result;
+	}
+	
+	public static final List<Float> newRange(final float first, final float step, final float last) {
+		final List<Float> result = new ArrayList<>();
+		
+		for (float i = first; first <= last ? i <= last : last <= i; i += step) {
+			result.add(i);
+		}
+		
+		return result;
+	}
+	
+	public static final List<Double> newRange(final double first, final double step, final double last) {
+		final List<Double> result = new ArrayList<>();
+		
+		for (double i = first; first <= last ? i <= last : last <= i; i += step) {
+			result.add(i);
+		}
+		
+		return result;
+	}
+	
+	public static final int[] toIntArray(final List<Integer> list) {
+		final int[] result = new int[list.size()];
 		
 		for (int i = 0; i < result.length; ++i) {
-			result[i] = resultBuilder.get(i);
+			result[i] = list.get(i);
+		}
+		
+		return result;
+	}
+	
+	public static final long[] toLongArray(final List<Long> list) {
+		final long[] result = new long[list.size()];
+		
+		for (int i = 0; i < result.length; ++i) {
+			result[i] = list.get(i);
+		}
+		
+		return result;
+	}
+	
+	public static final float[] toFloatArray(final List<Float> list) {
+		final float[] result = new float[list.size()];
+		
+		for (int i = 0; i < result.length; ++i) {
+			result[i] = list.get(i);
+		}
+		
+		return result;
+	}
+	
+	public static final double[] toDoubleArray(final List<Double> list) {
+		final double[] result = new double[list.size()];
+		
+		for (int i = 0; i < result.length; ++i) {
+			result[i] = list.get(i);
 		}
 		
 		return result;
--- test/multij/tools/CommandLineArgumentsParserTest.java
+++ test/multij/tools/CommandLineArgumentsParserTest.java
@@ -25,6 +25,8 @@
 package multij.tools;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 import multij.tools.CommandLineArgumentsParser;
 
 import org.junit.Test;
@@ -38,14 +40,24 @@ public final class CommandLineArgumentsParserTest {
     
     @Test
     public final void test1() {
-        final CommandLineArgumentsParser arguments = new CommandLineArgumentsParser("a", "42", "b", "1:2:5", "c", "0,-2,8:10", "e", "0x00FFFF", "f", "#FF000000");
+        final CommandLineArgumentsParser arguments = new CommandLineArgumentsParser("a", "42", "b", "1:2:5", "c", "0,-2,8:10", "e", "0x00FFFF", "f", "#FF000000", "g", "1:.5:2");
         
+        assertEquals(42L, arguments.get1("a", 42));
+        assertEquals(42L, arguments.get1("d", 42));
+        assertEquals(42L, arguments.get1("a", 42L));
+        assertEquals(42L, arguments.get1("d", 42L));
+        assertEquals(42.0, arguments.get1("a", 42F), 0.0);
+        assertEquals(42.0, arguments.get1("d", 42F), 0.0);
+        assertEquals(42.0, arguments.get1("a", 42.0), 0.0);
+        assertEquals(42.0, arguments.get1("d", 42.0), 0.0);
         assertArrayEquals(new int[] { 42 }, arguments.get("a", 41));
         assertArrayEquals(new int[] { 42 }, arguments.get("d", 42));
         assertArrayEquals(new int[] { 1, 3, 5 }, arguments.get("b"));
         assertArrayEquals(new int[] { 0, -2, 8, 9, 10 }, arguments.get("c"));
         assertArrayEquals(new int[] { 0x00FFFF }, arguments.get("e"));
         assertArrayEquals(new int[] { 0xFF000000 }, arguments.get("f"));
+        assertArrayEquals(new float[] { 1F, 1.5F, 2F }, arguments.get("g", 0F), 0F);
+        assertArrayEquals(new double[] { 1.0, 1.5, 2.0 }, arguments.get("g", 0.0), 0.0);
     }
     
 }
